e53a09c7e7aa361a6fa14b1e35ded99462980274
前端小丑 <lltfoo@163.com>
2021-03-25 09:12:28 +0800

    完成

--- src/views/menu/router.ts
+++ src/views/menu/router.ts
@@ -24,15 +24,7 @@ export default () => (
         meta: {
           name: '菜单二'
         }
-      },
-      {
-        path: '/home/index3',
-        name: 'index3',
-        component: () => import('./views/children/menu-3/index.vue'),
-        meta: {
-          name: '菜单二'
-        }
-      },
+      }
     ],
   }
 )
