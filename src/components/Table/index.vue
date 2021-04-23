7451e60b3f25b094eb5ac98efbac8cac767537db
llgtfoo <49134661+llgtfoo@users.noreply.github.com>
2021-04-23 16:43:48 +0800

    Update index.vue

--- src/components/Table/index.vue
+++ src/components/Table/index.vue
@@ -161,8 +161,9 @@ export default {
         showSizeChanger: true,
         showQuickJumper: true,
         total: this.total,
-        defaultCurrent: this.currentPage,
-        defaultPageSize: this.pageSize,
+        //defaultCurrent: this.currentPage,
+        //defaultPageSize: this.pageSize,
+        current: this.currentPage,
         pageSize: this.currentPageSize,
         pageSizeOptions: this.pageSizeOptions,
         showTotal: (total) => {
