01cbbcdeffb2fe050a65d0199636c5eecb3857b0
llgtfoo <49134661+llgtfoo@users.noreply.github.com>
2021-07-21 12:12:14 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <theme-provider
-    id='app'
-    :theme='theme'
+    id="app"
+    :theme="theme"
   >
     <a-config-provider :locale="locale">
       <div class="wrapper">
         <router-view v-slot="{ Component, route }">
-          <keep-alive>
+          <keep-alive v-if="Component&&Object.keys(Component).length>0">
             <component
               :is="Component"
               :route="route"
-            />
+            >
+            </component>
           </keep-alive>
+          <loading v-else></loading>
         </router-view>
       </div>
     </a-config-provider>
@@ -20,17 +22,19 @@
 
 <script lang="ts">
 import { defineComponent, ref, computed, getCurrentInstance } from 'vue'
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+import { useStore } from 'vuex'
 export default defineComponent({
   setup() {
-    const proxy: any = getCurrentInstance().proxy//获取App全局变量
+    const store = useStore()
+    // const { proxy } = getCurrentInstance()//获取App全局变量
 
     const theme = computed(() => {
-      return proxy.$store.getters['common/user/userTheme']
+      return store.getters['common/user/userTheme']
     })
 
     return { locale: ref(zhCN), theme }
-  }
+  },
 })
 </script>
 
@@ -42,4 +46,6 @@ export default defineComponent({
   position: relative;
 }
 </style>
+
+
 
