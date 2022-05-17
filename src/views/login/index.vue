b539c70630b1dda6e6e112230f6f32e255b31334
前端小丑 <lltfoo@163.com>
2022-05-17 21:02:55 +0800

    优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -7,10 +7,7 @@
       </div>
       <div class="login-right-form">
         <a-row style="width: 100%">
-          <a-col
-            :span="4.5"
-            :offset="3"
-          >
+          <a-col :span="4.5" :offset="3">
             <div class="form-title">账号登录</div>
           </a-col>
         </a-row>
@@ -22,11 +19,9 @@
           :rules="rules"
           :label-col="labelCol"
           :wrapper-col="wrapperCol"
+          layout="vertical"
         >
-          <a-form-item
-            label="用户名"
-            name="username"
-          >
+          <a-form-item label="用户名" name="username">
             <a-input
               v-model:value="formState.username"
               placeholder="请输入用户名"
@@ -37,10 +32,7 @@
               </template>
             </a-input>
           </a-form-item>
-          <a-form-item
-            label="密码"
-            name="password"
-          >
+          <a-form-item label="密码" name="password">
             <a-input
               v-model:value="formState.password"
               type="password"
@@ -53,7 +45,7 @@
           </a-form-item>
           <a-form-item name="type">
             <a-checkbox
-              :disabled="formState.username===''"
+              :disabled="formState.username === ''"
               @change="onChnage"
               v-model:checked="formState.checked"
               style="color: #fff"
@@ -62,12 +54,7 @@
             </a-checkbox>
           </a-form-item>
           <a-form-item :wrapper-col="{ span: 18, offset: 3 }">
-            <a-button
-              type="primary"
-              size=" large"
-              block
-              @click="onSubmit"
-            >
+            <a-button type="primary" size=" large" block @click="onSubmit">
               登录
             </a-button>
           </a-form-item>
@@ -77,9 +64,9 @@
   </div>
 </template>
 <script>
-import { defineComponent,reactive,ref,onMounted,watch } from 'vue'
-import { UserOutlined,LockOutlined } from '@ant-design/icons-vue'
-import { useRoute,useRouter } from 'vue-router'
+import { defineComponent, reactive, ref, onMounted, watch } from 'vue'
+import { UserOutlined, LockOutlined } from '@ant-design/icons-vue'
+import { useRoute, useRouter } from 'vue-router'
 export default defineComponent({
   components: {
     UserOutlined,
@@ -101,14 +88,14 @@ export default defineComponent({
       password: '1111',
       checked: false,
     })
-    watch(() => formState.username,(newValue) => {
+    watch(() => formState.username, (newValue) => {
       if (newValue === '') {
         formState.checked = false
       }
     })
     //   // 用户名自定义规则
     // eslint-disable-next-line no-unused-vars
-    const validateUsername = async (rule,value) => {
+    const validateUsername = async (rule, value) => {
       if (value === '') {
         return Promise.reject('用户名不能为空')
       } else {
@@ -117,7 +104,7 @@ export default defineComponent({
     }
     // 密码自定义规则
     // eslint-disable-next-line no-unused-vars
-    const validatePassword = async (rule,value) => {
+    const validatePassword = async (rule, value) => {
       if (value === '') {
         return Promise.reject('密码不能为空')
       } else {
@@ -126,13 +113,13 @@ export default defineComponent({
     }
     //校验规则
     const rules = {
-      username: [{ validator: validateUsername,trigger: 'change' }],
-      password: [{ validator: validatePassword,trigger: 'change' }],
+      username: [{ validator: validateUsername, trigger: 'change' }],
+      password: [{ validator: validatePassword, trigger: 'change' }],
     }
     //记住用户名操作
     const onChnage = function (e) {
       if (e.target.checked && formState.username !== '') {
-        localStorage.setItem('username',formState.username)
+        localStorage.setItem('username', formState.username)
       } else {
         localStorage.removeItem('username')
       }
@@ -140,24 +127,24 @@ export default defineComponent({
     //登录
     const onSubmit = function () {
       const root = formRef.value//$refs
-      console.log(root,'root')
+      console.log(root, 'root')
       root
         .validate()
         .then(() => {
-          console.log('values',formState)
-          localStorage.setItem('token','llgtfoo')
+          console.log('values', formState)
+          localStorage.setItem('token', 'llgtfoo')
           router.push('/home')
         })
         .catch((error) => {
-          console.log('error',error)
+          console.log('error', error)
         })
     }
     return {
       formRef,
       formState,
       rules,
-      labelCol: { span: 6,offset: 3 },
-      wrapperCol: { span: 18,offset: 3 },
+      labelCol: { span: 6, offset: 3 },
+      wrapperCol: { span: 18, offset: 3 },
       onSubmit,
       onChnage,
     }
